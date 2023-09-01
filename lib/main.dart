1e94137263572c931d920f2e7877ff6421cf765c
mjr2595 <michaeljohnraymond@gmail.com>
2023-08-31 23:15:54 -0500

    Added questions_screen and quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Colors.blue,
-                Colors.lightBlueAccent,
-              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-            ),
-            child: const StartScreen()),
-      ),
-    ),
+    const Quiz(),
   );
 }
