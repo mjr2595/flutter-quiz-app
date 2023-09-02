f79d159fd3d9b4341fc63eea7744024e6adfc3c5
mjr2595 <michaeljohnraymond@gmail.com>
2023-09-02 12:14:53 -0500

    Added: state for currentQuestionIndex

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -32,8 +40,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            ...currentQuestion.getShuffledAnswers().map(
-                (answer) => AnswerButton(answerText: answer, onTap: () {})),
+            ...currentQuestion.getShuffledAnswers().map((answer) =>
+                AnswerButton(answerText: answer, onTap: answerQuestion)),
           ],
         ),
       ),
